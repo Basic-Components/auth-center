0f6020189fb61043130da7c9b922005ab208de18
HUANG SIZHE <hsz1273327@gmail.com>
2017-08-16 11:32:46 +0800

    update

--- conf/testing.py
+++ conf/testing.py
@@ -2,4 +2,7 @@ from .default import DefaultSetting
 
 
 class TestingEnv(DefaultSetting):
-    SQLDBURLS = dict(defaultdb="mysql://root:hsz881224@127.0.0.1:3306/test_auth")
+    SQLDBURLS = dict(defaultdb="mysql://root:h7wFdCZN2NubZonbXAs1mYUf@114.55.125.148:3306/micro_auth")
+    REDIS_SETTINGS = dict(captcha="redis://:h7wFdCZN2NubZonbXAs1mYUf@114.55.125.148:6379/7", auth_token="redis://:h7wFdCZN2NubZonbXAs1mYUf@114.55.125.148:6379/7")
+    HOST = "0.0.0.0"
+    PORT = 4567
